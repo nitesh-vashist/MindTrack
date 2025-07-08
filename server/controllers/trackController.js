78cfbc450f2038f8d10cfe0d85e72bd6b81953c1
Nitesh <niteshvashist17@gmail.com>
2025-07-08 22:23:34 +0530

    Reorganize project: move client and add server backend

---
+++ server/controllers/trackController.js
@@ -0,0 +1,1 @@
+const Track  = require("/models/Track.js")
---
+++ server/models/Track.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const sessionSchema = new mongoose.Schema({
+    lectureNumber: Number,
+    lectureTitle: String,
+    duration: Number,
+    watchedTime: Number,
+    watchedDate: String,
+    status: String,
+    notes: String,
+    tags: [String],
+    resumePoint: String,
+});
+
+const trackSchema = new mongoose.Schema({
+    name: String,
+    type: String,
+    totalLectures: Number,
+    progress: Number,
+    isCompleted: Boolean,
+    lastUpdated: String,
+    tags: [String],
+    sessions: [sessionSchema],
+});
+
+const Track = mongoose.model("Track", trackSchema);
+module.exports = Track;
